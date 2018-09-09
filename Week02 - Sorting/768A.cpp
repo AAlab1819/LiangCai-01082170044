fb20d37cbd96e9523729f7cff04d17f5093f301c
Dave Hong <42820936+acailuv@users.noreply.github.com>
2018-09-09 13:21:53 +0700

    Update 768A.cpp

--- Week02 - Sorting/768A.cpp
+++ Week02 - Sorting/768A.cpp
@@ -1,60 +1,53 @@
-#include <bits/stdc++.h>
-
+// C++ implementation of Comb Sort
+#include<bits/stdc++.h>
 using namespace std;
 
-void shellSort(vector<int> arr) {
-    int n = arr.size();
-    for(unsigned int gap=n/2; gap>0; gap/=2)
-        for(int i=gap; i<n; i++)
-            for(int j=1-gap; j>=0; j-=gap)
-                if(arr[j] > arr[j+gap])
-                    swap(arr[j], arr[j+gap]);
+// To find gap between elements
+int getNextGap(int gap)
+{
+    // Shrink gap by Shrink factor
+    gap = (gap*10)/13;
+
+    if (gap < 1)
+        return 1;
+    return gap;
 }
 
-int getMax(int arr[], int size) {
-	int i, m = arr[0];
-	for(i = 1; i < size; i++) {
-		if(arr[i] > m) {
-			m = arr[i];
-		}
-	}
-	return m;
+// Function to sort a[0..n-1] using Comb Sort
+void combSort(int a[], int n)
+{
+    // Initialize gap
+    int gap = n;
+
+    // Initialize swapped as true to make sure that
+    // loop runs
+    bool swapped = true;
+
+    // Keep running while gap is more than 1 and last
+    // iteration caused a swap
+    while (gap != 1 || swapped == true)
+    {
+        // Find next gap
+        gap = getNextGap(gap);
+
+        // Initialize swapped as false so that we can
+        // check if swap happened or not
+        swapped = false;
+
+        // Compare all elements with current gap
+        for (int i=0; i<n-gap; i++)
+        {
+            if (a[i] > a[i+gap])
+            {
+                swap(a[i], a[i+gap]);
+                swapped = true;
+            }
+        }
+    }
 }
 
-void bucketSort(int arr[], int size) {
-	//variables
-	int max, bucket = 10, divider, i, j, k;
-
-	//10 buckets
-	vector<int> B[bucket];
-
-	//find max and min
-	max = getMax(arr, size);
-
-	divider = ceil(float(max + 1) / bucket);
-
-	//insert element into bucket
-	for(i = 0; i < size; i++) {
-		j = floor( arr[i] / divider );
-		B[j].push_back(arr[i]);
-	}
-
-	//sort elements in the buckets
-	for(i = 0; i < bucket; i++) {
-        //shellSort(B[i]);
-		sort(B[i].begin(), B[i].end());
-	}
-
-	//append back the elements from the buckets
-	k = 0;
-	for(i = 0; i < bucket; i++) {
-		for(j = 0; j < B[i].size(); j++) {
-			arr[k++] = B[i][j];
-		}
-	}
-}
-
-int main() {
+int main()
+{
     int n; cin >> n;//the number of stewards with Jon Snow
     int stewards[n]; //array containing stewards
     int lowestCount = 0; //this will contain the amount of lowest number in array
@@ -64,7 +57,7 @@ int main() {
         cin >> stewards[i];
     }
 
-    bucketSort(stewards, n);
+    combSort(stewards, n);
 
     //counting lowest number count
     for(int i=0; i<n; i++) {
